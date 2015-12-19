0da2f597e2e45b71371ba9920a815f92cd07b3fa
saibotd <contact@saibotd.com>
2015-12-19 18:47:32 +0100

    init

---
+++ android/src/com/gdxjam/magellan/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gdxjam.magellan.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gdxjam.magellan.MagellanGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MagellanGame(), config);
+	}
+}
---
+++ desktop/src/com/gdxjam/magellan/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gdxjam.magellan.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdxjam.magellan.MagellanGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MagellanGame(), config);
+	}
+}
