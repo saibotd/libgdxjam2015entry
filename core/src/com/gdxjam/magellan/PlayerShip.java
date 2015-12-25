fafbffa5c126a820dccfcdfa73e94d31a876a427
Felix Schittig <f.schittig@googlemail.com>
2015-12-26 00:21:04 +0100

    Nicer ship movement

--- core/src/com/gdxjam/magellan/PlayerShip.java
+++ core/src/com/gdxjam/magellan/PlayerShip.java
@@ -2,6 +2,8 @@ package com.gdxjam.magellan;
 
 import aurelienribon.tweenengine.Timeline;
 import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenEquation;
+import aurelienribon.tweenengine.TweenEquations;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -27,9 +29,9 @@ public class PlayerShip extends Ship {
 
 
         Timeline.createSequence()
-                .push(Tween.to(this.spriteShip, SpriteAccessor.ROTATION, 0.5f).target((float)Math.atan2(sector.position.y - lastSector.position.y, sector.position.x - lastSector.position.x)*180f/(float)Math.PI-90f))
-                .push(Tween.to(this.spriteShip, SpriteAccessor.POSITION_XY, 0.7f).target(sector.position.x + 20, sector.position.y - 30))
-                .push(Tween.to(this.spriteShip, SpriteAccessor.ROTATION, 0.5f).target(0))
+                .push(Tween.to(this.spriteShip, SpriteAccessor.ROTATION, 0.3f).target((float)Math.atan2(sector.position.y - lastSector.position.y, sector.position.x - lastSector.position.x)*180f/(float)Math.PI-90f))
+                .push(Tween.to(this.spriteShip, SpriteAccessor.POSITION_XY, 0.5f).target(sector.position.x + 20, sector.position.y - 30).ease(TweenEquations.easeInOutQuint))
+                //.push(Tween.to(this.spriteShip, SpriteAccessor.ROTATION, 0.5f).target(0).delay(-0.1f).ease(TweenEquations.easeInOutCubic))
         .start(tweenManager);
 
 
