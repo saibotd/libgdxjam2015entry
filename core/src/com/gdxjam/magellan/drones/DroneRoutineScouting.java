859d845fde4ab0eff79f23ef3a867d20ebe543c9
saibotd <contact@saibotd.com>
2015-12-28 23:38:24 +0100

    Scouting drones discover new sectors now

--- core/src/com/gdxjam/magellan/drones/DroneRoutineScouting.java
+++ core/src/com/gdxjam/magellan/drones/DroneRoutineScouting.java
@@ -1,5 +1,7 @@
 package com.gdxjam.magellan.drones;
 
+import com.gdxjam.magellan.Sector;
+
 /**
  * Created by saibotd on 27.12.15.
  */
@@ -15,7 +17,13 @@ public class DroneRoutineScouting extends DroneRoutine{
     public void tick(){
         wait -= powerLevel;
         if(wait <= 0) {
-            drone.moveTo(drone.sector.connectedSectors.random());
+            Sector sector = drone.sector.connectedSectors.random();
+            sector.discovered = true;
+            sector.visited = true;
+            for(Sector _sector: sector.connectedSectors){
+                _sector.discovered = true;
+            }
+            drone.moveTo(sector);
             wait = 5;
         }
     }
