65ec44452498e4ff7e39db31c7e488d697cbcbd3
Tobias Dühr <nerd@tobiasduehr.com>
2016-01-16 11:52:07 +0100

    Drones only die once, Log order

--- core/src/com/gdxjam/magellan/ships/AiShipSettler.java
+++ core/src/com/gdxjam/magellan/ships/AiShipSettler.java
@@ -93,13 +93,13 @@ public class AiShipSettler extends AiShip {
     public void activeTick(){
         decideState();
         if(state == States.HOSTILE){
-            new Battle(this, target);
             if (target instanceof Drone && ((Drone) target).faction == Factions.PLAYER) {
                 MagellanGame.instance.mapScreen.log.addEntry("Your drone is under attack!", sector);
             }
             if (target instanceof Planet && ((Planet) target).faction == Factions.PLAYER) {
                 MagellanGame.instance.mapScreen.log.addEntry("Your planet is under attack!", sector);
             }
+            new Battle(this, target);
         }
     }
 
